c9070ea692c12b0b3ea0f00e353ce6d9793d09cf
Wolfgang Malgadey <wolfgang@malgadey.de>
2025-04-03 23:42:59 +0200

    fix again, what has already been fixed in #183

--- PyTado/interface/api/hops_tado.py
+++ PyTado/interface/api/hops_tado.py
@@ -62,7 +62,7 @@ class TadoX(Tado):
         for device in devices:
             request = TadoXRequest()
             request.domain = Domain.DEVICES
-            request.device = device["serialNumber"]
+            request.device = device["serialNo"]
             device.update(self._http.request(request))
 
         if "otherDevices" in rooms_and_devices:
