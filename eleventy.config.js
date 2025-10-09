b716ce46d4cf0cc901f2644857f9fe63754c678f
Wolfgang Malgadey <wolfgang@malgadey.de>
2025-10-10 01:06:12 +0200

    different approach

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,8 @@ import pluginFilters from "./docs/_config/filters.js";
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
+	eleventyConfig.setUseGitIgnore(false);
+
 	// Drafts, see also _data/eleventyDataSchema.js
 	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
 		if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
